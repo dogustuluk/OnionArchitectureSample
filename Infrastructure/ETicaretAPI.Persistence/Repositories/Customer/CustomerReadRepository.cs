80de25a88de0b44659e0519e9cd0878f3d619d3d
dogustuluk <dogus.tuluk@gmail.com>
2023-10-07 12:07:38 +0300

    generic repository design pattern ve marker pattern uygulaması

---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/Customer/CustomerReadRepository.cs
@@ -0,0 +1,20 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
+using ETicaretAPI.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories
+{
+    /*ReadRepository<Customer>
+     ReadRepository<Customer>'i kaldırırsak ICustomerReadRepository içerisindekileri tekrardan yazmamız lazım.*/
+    public class CustomerReadRepository : ReadRepository<Customer>, ICustomerReadRepository
+    {
+        public CustomerReadRepository(ETicaretAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
