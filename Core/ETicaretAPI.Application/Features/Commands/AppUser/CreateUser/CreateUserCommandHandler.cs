0a16ff5b73377c56dcf756161faa78d407c0ce09
dogustuluk <dogus.tuluk@gmail.com>
2024-05-27 13:18:45 +0300

    identity yapısı ve kullanıcı kaydı

---
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using appuser = ETicaretAPI.Domain.Entities.Identity;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaretAPI.Application.Exceptions;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.CreateUser
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommandRequest, CreateUserCommandResponse>
+    {
+        readonly UserManager<appuser.AppUser> _userManager;
+
+        public CreateUserCommandHandler(UserManager<appuser.AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<CreateUserCommandResponse> Handle(CreateUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            IdentityResult result = await _userManager.CreateAsync(new()
+            {
+                Id = Guid.NewGuid().ToString(),
+                NameSurname = request.NameSurname,
+                UserName = request.UserName,
+                Email = request.Email,
+            }, request.Password);
+
+
+            CreateUserCommandResponse response = new() { Succeeded = result.Succeeded };
+            if (result.Succeeded)
+                response.Message = "Kullanıcı Kaydı Başarılı";
+            else
+                foreach (var error in result.Errors)
+                    response.Message += $"{error.Code}-{error.Description}\n";
+
+            return response;
+            // throw new UserCreateFailedException();
+
+        }
+    }
+}
--- Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities.Identity;
 using ETicaretAPI.Persistence.Contexts;
 using ETicaretAPI.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -17,6 +18,16 @@ namespace ETicaretAPI.Persistence
 		public static void AddPersistenceServices(this IServiceCollection services)
 		{
 			services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+            services.AddIdentity<AppUser, AppRole>(options =>
+            {
+                //for test mode
+                options.Password.RequiredLength = 3;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            }).AddEntityFrameworkStores<ETicaretAPIDbContext>();//identity
+
 			services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
 			services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
             services.AddScoped<IOrderReadRepository, OrderReadRepository>();
