edd26d937947788e80ad28c148ee50aa827a2085
dogustuluk <dogus.tuluk@gmail.com>
2024-05-30 17:41:43 +0300

    facebook login

---
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/FacebookLogin/FacebookLoginCommandHandler.cs
@@ -0,0 +1,77 @@
+﻿using ETicaretAPI.Application.Abstractions.Token;
+using ETicaretAPI.Application.DTOs;
+using ETicaretAPI.Application.DTOs.Facebook;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.FacebookLogin
+{
+    public class FacebookLoginCommandHandler : IRequestHandler<FacebookLoginCommandRequest, FacebookLoginCommandResponse>
+    {
+        readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
+        readonly ITokenHandler _tokenHandler;
+        readonly HttpClient _httpClient; //DI'dan çağırıyoruz çünkü herhangi bir bağımlılık istemiyoruz.
+        readonly IConfiguration _configuration;
+        public FacebookLoginCommandHandler(UserManager<Domain.Entities.Identity.AppUser> userManager, ITokenHandler tokenHandler, HttpClient httpClient, IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _tokenHandler = tokenHandler;
+            _httpClient = httpClientFactory.CreateClient();
+            _configuration = configuration;
+        }
+
+        public async Task<FacebookLoginCommandResponse> Handle(FacebookLoginCommandRequest request, CancellationToken cancellationToken)
+        {
+            string accessTokenResponse = await _httpClient.GetStringAsync($"https://graph.facebook.com/oauth/access_token?client_id={_configuration["FacebookLogin:ClientId"]}&client_secret={_configuration["FacebookLogin:ClientSecret"]}&grant_type=client_credentials");
+
+            FacebookAccessTokenResponse facebookAccessTokenResponse = JsonSerializer.Deserialize<FacebookAccessTokenResponse>(accessTokenResponse);
+
+            string userAccessTokenValidation = await _httpClient.GetStringAsync($"https://graph.facebook.com/debug_token?input_token={request.AuthToken}&access_token={facebookAccessTokenResponse.AccessToken}");
+
+            FacebookUserAccessTokenValidationData validation = JsonSerializer.Deserialize<FacebookUserAccessTokenValidationData>(userAccessTokenValidation);
+
+            if (validation.Data.IsValid)
+            {
+                string userInfoResponse = await _httpClient.GetStringAsync($"https://graph.facebook.com/me?fields=email,name&access_token={request.AuthToken}");
+
+                FacebookUserInfoResponse userInfo = JsonSerializer.Deserialize<FacebookUserInfoResponse>(userInfoResponse);
+
+                var info = new UserLoginInfo("FACEBOOK", validation.Data.UserId, "FACEBOOK"); //AspNetUserLogins tablosuna kaydetmek için gerekli nesne oluşturulur.
+
+                Domain.Entities.Identity.AppUser user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+
+                bool result = user != null;
+                if (user == null)
+                {
+                    user = await _userManager.FindByEmailAsync(userInfo.Email);
+                    if (user == null)
+                    {
+                        user = new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Email = userInfo.Email,
+                            UserName = userInfo.Email,
+                            NameSurname = userInfo.Name
+                        };
+
+                        var identityResult = await _userManager.CreateAsync(user); //asp net users tablosuna kayıt edildi.
+                        result = identityResult.Succeeded;
+                    }
+                }
+                if (result)
+                {
+                    await _userManager.AddLoginAsync(user, info);
+                    Token token = _tokenHandler.CreateAccessToken(60);
+                    return new()
+                    {
+                        Token = token
+                    };
+                }
+            }           
+            throw new Exception("invalid external authentication");
+
+        }
+    }
+}
